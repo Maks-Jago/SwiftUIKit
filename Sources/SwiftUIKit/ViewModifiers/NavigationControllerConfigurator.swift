4567ec7af1259ccf63be031c3585be7ef2e9acc8
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2020-11-19 17:22:54 +0200

    add navigation controller configurator

---
+++ Sources/SwiftUIKit/ViewModifiers/NavigationControllerConfigurator.swift
@@ -0,0 +1,22 @@
+//
+//  NavigationControllerConfigurator.swift
+//  
+//
+//  Created by Max Kuznetsov on 19.11.2020.
+//
+
+import SwiftUI
+
+struct NavigationConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void = { _ in }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<NavigationConfigurator>) -> UIViewController {
+        UIViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavigationConfigurator>) {
+        if let nc = uiViewController.navigationController {
+            self.configure(nc)
+        }
+    }
+}
--- Sources/SwiftUIKit/Views/NavigationControllerConfigurator.swift
+++ Sources/SwiftUIKit/Views/NavigationControllerConfigurator.swift
@@ -9,22 +9,22 @@ import SwiftUI
 
 public extension View {
     func configureNavigationController(_ configure: @escaping (UINavigationController) -> Void) -> some View {
-        self.background(NavigationControllerConfigurator(configure: configure))
+        background(NavigationControllerConfigurator(configure: configure))
     }
 }
 
-public struct NavigationControllerConfigurator: UIViewControllerRepresentable {
-    public var configure: (UINavigationController) -> Void
+struct NavigationControllerConfigurator: UIViewControllerRepresentable {
+    var configure: (UINavigationController) -> Void
     
-    public init(configure: @escaping (UINavigationController) -> Void) {
+    init(configure: @escaping (UINavigationController) -> Void) {
         self.configure = configure
     }
     
-    public func makeUIViewController(context: Context) -> UIViewController {
+    func makeUIViewController(context: Context) -> UIViewController {
         UIViewController()
     }
     
-    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         if let nc = uiViewController.navigationController {
             self.configure(nc)
         }
