b354b11e03b2e68035c95f64a7ecb332a6e89542
vfil <wladyslawfil@gmail.com>
2021-09-23 19:11:13 +0300

    Fix UIFontTextStyle

--- Sources/SwiftUI-Kit/Extensions/UIFontTextStyle.swift
+++ Sources/SwiftUI-Kit/Extensions/UIFontTextStyle.swift
@@ -7,18 +7,21 @@
 
 import SwiftUI
 
-public extension UIFont.TextStyle {
-    
+@available(iOS 14, *)
+extension UIFont.TextStyle {
     init?(_ textStyle: Font.TextStyle) {
         let styles: [Font.TextStyle: UIFont.TextStyle] = [
             .largeTitle: .largeTitle,
             .title: .title1,
+            .title2: .title2,
+            .title3: .title3,
             .headline: .headline,
             .subheadline: .subheadline,
             .body: .body,
             .callout: .callout,
             .footnote: .footnote,
-            .caption: .caption1
+            .caption: .caption1,
+            .caption2: .caption2
         ]
         
         guard let style = styles[textStyle] else {
