8a3e3b5a219bcb540abb4517922dded7d0b75ae5
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2023-06-25 14:55:56 +0300

    Update NavigationLink.swift

--- Sources/SwiftUI-Kit/Extensions/NavigationLink.swift
+++ Sources/SwiftUI-Kit/Extensions/NavigationLink.swift
@@ -8,11 +8,8 @@
 import SwiftUI
 
 public extension NavigationLink where Label == EmptyView {
-    init(destination: Destination, isActive: Binding<Bool>) {
-        self.init(destination: destination, isActive: isActive) { EmptyView() }
-    }
-    
+
     init(isActive: Binding<Bool>, @ViewBuilder destination: @escaping () -> Destination) {
-        self.init(destination: destination(), isActive: isActive) { EmptyView() }
+        self.init(isActive: isActive, destination: destination) { EmptyView() }
     }
 }
