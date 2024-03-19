c4e306bff6a2f36f348526f862c50f44b40673b2
Arthur Zavolovych <boredarthur@gmail.com>
2024-03-19 20:07:08 +0200

    View embedding extension

--- Sources/SwiftUI-Kit/Extensions/View+Embed.swift
+++ Sources/SwiftUI-Kit/Extensions/View+Embed.swift
@@ -37,4 +37,28 @@ public extension View {
             self
         }
     }
+    
+    @available(iOS 16.0, *)
+    func embedInNavigationLink<P: Hashable>(value: P?) -> some View {
+        NavigationLink(value: value, label: { self })
+    }
+    
+    func embedInNavigationLink<V: View>(destination: () -> V) -> some View {
+        NavigationLink(destination: destination, label: { self })
+    }
+    
+    @available(iOS 16.0, *)
+    func embedInNavigationStack() -> some View {
+        NavigationStack { self }
+    }
+    
+    @available(iOS 16.0, *)
+    func embedInNavigationStack<Data>(with path: Binding<Data>) -> some View where Data : MutableCollection, Data : RandomAccessCollection, Data : RangeReplaceableCollection, Data.Element : Hashable {
+        NavigationStack(path: path) { self }
+    }
+    
+    func embedInPlainButton(action: @escaping () -> Void) -> some View {
+        Button(action: action, label: { self })
+            .buttonStyle(.plain)
+    }
 }
